814a2ba788072b3dcc66c7d3ca75ca3aaf07c4c2
earayu <earayu@163.com>
2024-07-03 02:42:21 +0800

    update

--- examples/datamocking/main.go
+++ examples/datamocking/main.go
@@ -24,18 +24,25 @@ func (a *DataMockingWasmPlugin) RunAfterExecution(queryResult *query.QueryResult
 	}
 
 	result := sqltypes.Proto3ToResult(queryResult)
-	for _, row := range result.Rows {
-		for _, value := range row {
-			switch value.Type() {
-			case sqltypes.VarChar, sqltypes.Char, sqltypes.Text:
-				value = sqltypes.MakeTrusted(sqltypes.VarChar, []byte("foobar"))
-			case sqltypes.Int8, sqltypes.Int16, sqltypes.Int24, sqltypes.Int32, sqltypes.Int64:
-				value = sqltypes.NewInt8(42)
-			case sqltypes.Uint8, sqltypes.Uint16, sqltypes.Uint24, sqltypes.Uint32, sqltypes.Uint64:
-				value = sqltypes.NewUint64(42)
+	mockResult := sqltypes.MakeTestResult(result.Fields)
+	if len(result.Rows) > 0 {
+		mockResult.Rows = make([][]sqltypes.Value, len(result.Rows))
+	}
+	for i, row := range result.Rows {
+		mockResult.Rows[i] = make([]sqltypes.Value, len(mockResult.Fields))
+		for j, field := range mockResult.Fields {
+			switch field.Type {
+			case query.Type_VARCHAR, query.Type_CHAR, query.Type_TEXT:
+				mockResult.Rows[i][j] = sqltypes.MakeTrusted(field.Type, []byte("mocked"))
+			case query.Type_INT8, query.Type_INT16, query.Type_INT24, query.Type_INT32, query.Type_INT64:
+				mockResult.Rows[i][j] = sqltypes.MakeTrusted(field.Type, []byte("42"))
+			case query.Type_UINT8, query.Type_UINT16, query.Type_UINT24, query.Type_UINT32, query.Type_UINT64:
+				mockResult.Rows[i][j] = sqltypes.MakeTrusted(field.Type, []byte("42"))
+			default:
+				mockResult.Rows[i][j] = row[j]
 			}
 		}
 	}
 
-	return queryResult, errBefore
+	return sqltypes.ResultToProto3(mockResult), errBefore
 }
